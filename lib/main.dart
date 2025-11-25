ccb7653de7865b8c127a9ce0e2d89ec67f3df7a8
TheFlutterSensei <hi@fluttersensei.com>
2025-11-25 20:10:55 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello World Toggle',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.amber),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _isHello = true;
+
+  void _toggleHello() {
+    setState(() {
+      _isHello = !_isHello;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Hello World!'),
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Theme.of(context).colorScheme.onPrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              _isHello ? 'Hello Flutter!' : 'You Clicked the Button!',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: _toggleHello,
+              child: Text(_isHello ? 'Change Text' : 'Change Back'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
